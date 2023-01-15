3488fdd242303027d392c85abae2b4dad07dae06
sladesladeslade <spbrooks4@gmail.com>
2023-01-15 03:00:50 -0500

    broken

--- clothSim/cloth.py
+++ clothSim/cloth.py
@@ -1,28 +1,29 @@
-import pybullet as p
-import numpy as np
+import pymunk
+import pymunk.pygame_util
 
 # Create the simulation
-physicsClient = p.connect(p.DIRECT)
-p.setGravity(0,0,-10)
+space = pymunk.Space()
+space.gravity = (0,0)
 
 # Create a grid of point masses
 num_points = 5
 points = []
 for i in range(num_points):
     for j in range(num_points):
-        point = p.createMultiBody(baseMass=1, baseInertialFramePosition=[i, j, 0])
+        point = pymunk.Body(mass=float("999"), moment=0)
+        point.position = (i, j)
+        space.add(point)
         points.append(point)
 
 # Connect the point masses with distance constraints
 for i in range(num_points*num_points):
     if (i+1) % num_points != 0:
-        p.createConstraint(points[i], -1, points[i+1], -1, p.JOINT_POINT2POINT, [0,0,0], [1,0,0], [0,0,0])
+        constraint = pymunk.PinJoint(points[i], points[i+1], (0,0), (1,0))
+        space.add(constraint)
     if i+num_points < num_points*num_points:
-        p.createConstraint(points[i], -1, points[i+num_points], -1, p.JOINT_POINT2POINT, [0,0,0], [0,1,0], [0,0,0])
+        constraint = pymunk.PinJoint(points[i], points[i+num_points], (0,0), (0,1))
+        space.add(constraint)
 
 # Step the simulation
 for i in range(1000):
-    p.stepSimulation()
-
-# Disconnect and clean up
-p.disconnect()
+    space.step(1/60)
